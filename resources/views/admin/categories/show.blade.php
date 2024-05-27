63badc275a9f700a650c149e1a2620e2b54b6e64
alex02r <alex.giochi02@gmail.com>
2024-05-27 14:03:18 +0200

    upgrade: creazione pagina show della categoria

---
+++ resources/views/admin/categories/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row row-gap-4 justify-content-center">
+            <div class="col-12">
+                <h1>Categoria: ...</h1>
+            </div>
+        </div>
+    </div>  
+@endsection
