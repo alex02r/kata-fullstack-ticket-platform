043122bea124041e0d9a0bc49451400249f38cef
alex02r <alex.giochi02@gmail.com>
2024-05-27 14:12:29 +0200

    upgrade: mostrate informazioni della categoria

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -4,7 +4,17 @@
     <div class="container">
         <div class="row row-gap-4 justify-content-center">
             <div class="col-12">
-                <h1>Categoria: ...</h1>
+                <h1>Visualizzazione Categoria</h1>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="card mb-3">
+                    <div class="card-body">
+                      <h3 class="card-title">{{ $category->name}}</h3>
+                      <p class="card-text">Id della categoria: {{ $category->id }}</p>
+                      <a href="#" class="btn btn-warning">Modifica</a>
+                      <a href="#" class="btn btn-danger">Elimina</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>  
