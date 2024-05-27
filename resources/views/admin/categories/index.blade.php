97aa4478d907af400566dfc72667c5bde5c04b6e
alex02r <alex.giochi02@gmail.com>
2024-05-27 14:01:29 +0200

    upgrade: visualizzazione tabella delle categorie

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -2,9 +2,29 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row row-gap-4 justify-content-center">
             <div class="col-12">
                 <h1>Lista categorie</h1>
+            </div>
+            <div class="col-12 col-md-8">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Tools</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($categories as $category)
+                            <tr>
+                                <td>{{ $category->id }}</td>
+                                <td>{{ $category->name }}</td>
+                                <td>...</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>  
