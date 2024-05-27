fc1459bc24d7fed81a50eb89591cebc429baf165
alex02r <alex.giochi02@gmail.com>
2024-05-27 14:36:43 +0200

    upgrade: form di creazione nuova categoria

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -2,12 +2,28 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row row-gap-4">
             <div class="col-12">
                 <h1>Creazione nuova categoria</h1>
             </div>
-            <div class="col-12">
-                
+            <div class="col-12 col-md-8">
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+                <form method="POST" action="{{route('admin.categories.store')}}">
+                    @csrf
+                    @method('POST')
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Inserisci il nome della categoria</label>
+                        <input type="text" id="name" name="name" class="form-control w-50">
+                    </div>
+                </form>
             </div>
         </div>
     </div>
