91c3ee3d1a790739647d7075a6114317c712c5ef
alex02r <alex.giochi02@gmail.com>
2024-05-27 15:33:16 +0200

    upgrade: funzione destroy

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -80,6 +80,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        
+        $category->delete();
+        return redirect()->route('admin.categories.index');
     }
 }
