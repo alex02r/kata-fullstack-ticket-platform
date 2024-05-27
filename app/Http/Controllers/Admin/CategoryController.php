472eeb0948ebb70f83bc8a3ffc8c7c83500bf12c
alex02r <alex.giochi02@gmail.com>
2024-05-27 14:03:58 +0200

    upgrade: definizione funzione show

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -38,7 +38,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        
+        return view("admin.categories.show", compact("category"));
     }
 
     /**
