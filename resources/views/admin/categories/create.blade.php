b9915e2b9d83838a10bc50215e77ebe96751bbdc
alex02r <alex.giochi02@gmail.com>
2024-05-27 14:14:56 +0200

    upgrade: pagina create di categoria

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Creazione nuova categoria</h1>
+            </div>
+            <div class="col-12">
+                
+            </div>
+        </div>
+    </div>
+@endsection
