12b7e240e06fadd5da8b75de56847f46f310a02c
alex02r <alex.giochi02@gmail.com>
2024-05-27 14:39:07 +0200

    upgrade: aggiunto bottone per la creazione e per tornare indietro

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -5,6 +5,7 @@
         <div class="row row-gap-4">
             <div class="col-12">
                 <h1>Creazione nuova categoria</h1>
+                <a href="{{ route('admin.categories.index') }}" class="btn btn-primary">Torna indietro</a>
             </div>
             <div class="col-12 col-md-8">
                 @if ($errors->any())
@@ -21,8 +22,11 @@
                     @method('POST')
                     <div class="mb-3">
                         <label for="name" class="form-label">Inserisci il nome della categoria</label>
-                        <input type="text" id="name" name="name" class="form-control w-50">
+                        <input type="text" id="name" name="name" class="form-control w-50" required>
                     </div>
+                    <button type="submit" class="btn btn-success">
+                        Crea
+                    </button>
                 </form>
             </div>
         </div>
