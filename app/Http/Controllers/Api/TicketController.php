80ce9efdab90f761e8825a5c45598cf850829933
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-05-31 17:11:20 +0200

    feat: api controller and routes

--- app/Http/Controllers/Api/TicketController.php
+++ app/Http/Controllers/Api/TicketController.php
@@ -20,4 +20,40 @@ class TicketController extends Controller
         ]);
     }
 
+    public function search(Request $request)
+    {
+        $query = Ticket::with('category', 'operator', 'status', 'priority');
+    
+        if ($request->has('date') && $request->input('date') != null) {
+            $query->whereDate('date', $request->input('date'));
+        }
+    
+        if ($request->has('status') && $request->input('status') != null) {
+            $query->where('status_id', $request->input('status'));
+        }
+    
+        if ($request->has('category') && $request->input('category') != null) {
+            $query->where('category_id', $request->input('category'));
+        }
+    
+        if ($request->has('operator_id') && $request->input('operator_id') != null) {
+            $query->where('operator_id', $request->input('operator_id'));
+        }
+    
+        if (!$request->has('date') && !$request->has('status') && !$request->has('category') && !$request->has('operator_id')) {
+            return response()->json(['error' => 'Devi applicare almeno un filtro'], 400);
+        }
+    
+        // Debugga la query
+        // dd($query->toSql(), $query->getBindings());
+    
+        $tickets = $query->paginate(12);
+    
+        return response()->json($tickets);
+    }
+    
+    
+
+    
+
 }
